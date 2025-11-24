52379120d62be29936eed67a1fa2866dc1ecbd05
prit007p <pritbaldaniya@gmail.com>
2025-11-24 18:03:22 +0530

    added

--- backend/routes/chats.js
+++ backend/routes/chats.js
@@ -19,6 +19,7 @@ router.get('/',async (req,res)=>{
         res.json(friendsWithLastMessage);
     }    
     catch(err){
+        console.log("k");
         console.log("error in finding friends in Chats",err);
     }
 })
