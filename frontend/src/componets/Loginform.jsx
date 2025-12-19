346c4ce7833ac8af9c74928f7c130e8aebc0f876
prit007p <pritbaldaniya@gmail.com>
2025-12-19 19:03:28 +0530

    feat: Add Loginform component for user authentication with email and password.

--- frontend/src/componets/Loginform.jsx
+++ frontend/src/componets/Loginform.jsx
@@ -93,7 +93,7 @@ const LoginPage =  function LoginForm({
             </div>
             <div className="mt-4 text-center text-sm">
               Don&apos;t have an account?{" "}
-              <a href="#" className="underline underline-offset-4">
+              <a href="/register" className="underline underline-offset-4">
                 Sign up
               </a>
             </div>
